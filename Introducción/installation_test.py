5ffc520c878b1958c4204083ef4a62355bcac089
Unknown <daniel.garcia@banlinea.com>
2017-08-01 17:40:48 -0500

    First Sesion

---
+++ Introducción/installation_test.py
@@ -0,0 +1,13 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+
+sess = tf.Session()
+
+hello = tf.constant('Prueba TensorFlow')
+print(sess.run(hello))
+
+a = tf.constant(20)
+b = tf.constant(22)
+print('a + b = {0}'.format(sess.run(a+b)))
